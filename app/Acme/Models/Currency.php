d82f0ba7a62c49366f7aed4e79803f11be99ddcb
Sudip Limbu <responsivesudip@gmail.com>
2019-10-01 13:28:15 +0545

    Round decimal on currency

--- app/Acme/Models/Currency.php
+++ app/Acme/Models/Currency.php
@@ -28,20 +28,20 @@ class Currency extends Model
     {
         $coinModel = $this->where('currency', 'Coin')->first();
 
-        return $bits / $coinModel->value_in_bits;
+        return round($bits / $coinModel->value_in_bits);
     }
 
     public function btcToBits($btc)
     {
         $btcModel = $this->where('currency', 'BTC')->first();
 
-        return $btc * $btcModel->value_in_bits;
+        return ;
     }
 
     public function bitsToBtc($bits)
     {
         $btcModel = $this->where('currency', 'BTC')->first();
 
-        return $bits / $btcModel->value_in_bits;
+        return round($bits / $btcModel->value_in_bits, 6);
     }
 }
